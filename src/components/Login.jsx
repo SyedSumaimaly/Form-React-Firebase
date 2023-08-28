0400e475e3b3aaac6ef2ae1e9f70a754fc4e65cb
SyedSumaimaly <syedsumaimaly08@gmailo.com>
2023-08-28 18:49:53 +0100

    Second Commit

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -18,8 +18,32 @@ function Login() {
   });
 
   const [errorMsg, setErrorMsg] = useState("");
-
+  const [emailmsg, SetemailMsg] = useState();
+  const [passmsg, SetpassMsg] = useState();
   const navigate = useNavigate();
+
+  const handleEmail = (event) => {
+    setValues((prev) => ({ ...prev, email: event.target.value }))
+    let patt = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (values.email.match(patt)) {
+      SetemailMsg("Valid email address!")
+    } else (
+      SetemailMsg("Your Email must contain English letters, Digits (0-9) and Special Characters")
+    )
+  }
+
+  const handlePassword = (e) => {
+    setValues((prev) => ({ ...prev, pass: e.target.value }))
+
+    let passreg = /^[A-Za-z]\w{7,14}$/;
+
+    if (values.pass.match(passreg)) {
+      SetpassMsg("Correct Password");
+    } else {
+      SetpassMsg("Your Password must contain 7 to 16 charactersc including numeric digits, underscore and first character must be a letter")
+    }
+
+  }
 
   const handleLogin = () => {
     if (!values.email || !values.pass) {
@@ -44,12 +68,10 @@ function Login() {
     <div className='maintwo'>
       <h1>Login</h1>
       <div>
-        <Input placeholder="Enter your email" onChange={(event) =>
-          setValues((prev) => ({ ...prev, email: event.target.value }))
-        } />
-        <Input placeholder="Enter your password" onChange={(event) =>
-          setValues((prev) => ({ ...prev, pass: event.target.value }))
-        } />
+        <Input placeholder="Enter your email" onChange={handleEmail} />
+        <p className={emailmsg == "Valid email address!" ? "validcolor" : "errorcolor"}>{emailmsg}</p>
+        <Input placeholder="Enter your password" onChange={handlePassword} />
+        <p className={passmsg == "Correct Password" ? "validcolor" : "errorcolor"}>{passmsg}</p>
       </div>
       <div className='error'>
         {errorMsg}
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -12,11 +12,33 @@ function Signup() {
         email: "",
         password: "",
     });
+
     const [errorMessage, SeterrorMessage] = useState("")
     const navigate = useNavigate();
+    const [emailmsg, SetemailMsg] = useState();
+    const [passmsg, SetpassMsg] = useState();
 
     const handleEmail = (event) => {
         setValues((prev) => ({ ...prev, email: event.target.value }))
+        let patt = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        if (values.email.match(patt)) {
+            SetemailMsg("Valid email address!")
+        } else (
+            SetemailMsg("Your Email must contain English letters, Digits (0-9) and Special Characters")
+        )
+    }
+
+    const handlePassword = (e) => {
+        setValues((prev) => ({ ...prev, password: e.target.value }))
+
+        let pass = /^[A-Za-z]\w{7,14}$/;
+
+        if (values.password.match(pass)) {
+            SetpassMsg("Correct Password")
+        } else {
+            SetpassMsg("Your Password must contain  7 to 16 characters including characters, numeric digits, underscore and first character must be a letter")
+        }
+
     }
 
     const handleSubmit = () => {
@@ -48,7 +70,9 @@ function Signup() {
             <div>
                 <Input placeholder="Enter your name" onChange={(event) => setValues((prev) => ({ ...prev, name: event.target.value }))} />
                 <Input placeholder="Enter your email" onChange={handleEmail} />
-                <Input placeholder="Enter your password" onChange={(event) => setValues((prev) => ({ ...prev, password: event.target.value }))} />
+                <p className={emailmsg == "Valid email address!" ? "validcolor" : "errorcolor"}>{emailmsg}</p>
+                <Input placeholder="Enter your password" onChange={handlePassword} />
+                <p className={passmsg == "Correct Password" ? "validcolor" : "errorcolor"}>{passmsg}</p>
                 <div className='error'>{errorMessage}</div>
             </div>
             <Button type="primary" onClick={handleSubmit}>Sign Up</Button>
